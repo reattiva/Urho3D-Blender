382e897bf9b30674d06a1c2df24f836fda2ec199
Yusuf Kağan Hanoğlu <hanoglu@yahoo.com>
2023-02-12 00:15:27 +0300

    Add files via upload

--- export_urho.py
+++ export_urho.py
@@ -1259,7 +1259,7 @@ def UrhoExport(tData, uExportOptions, uExportData, errorsMem):
             if tMaterial.alphaMask:
                 uMaterial.psdefines += " ALPHAMASK"
         if "metallic" in tMaterial.texturesNames or "roughness" in tMaterial.texturesNames: # YKH
-           technique = "PBRMetallicRough" # YKH
+            technique = "PBRMetallicRough" # YKH
             if "diffuse" in tMaterial.texturesNames: # YKH
                 technique += "Diff" # YKH
             if "normal" in tMaterial.texturesNames: #YKH
